7bb3de9634aed1622f53a1041ca5af947683f829
jesuslerma <shuyojl@gmail.com>
2018-10-03 17:11:45 -0500

    Send new todo to server

--- Todo.js
+++ Todo.js
@@ -16,14 +16,30 @@ export class Todo extends Component {
     }
   }
 
+  async componentWillMount () {
+    // the ip value will change
+    const response = await fetch('http://192.168.15.8:3011/todos')
+    const todos = await response.json()
+    this.setState({todos})
+  }
+
   handleChange (text) {
     this.setState({newTodo: text})
   }
 
-  handlePress () {
+  async handlePress () {
     const newTodo = {
       name: this.state.newTodo
     }
+    // the ip value will change
+    await fetch('http://192.168.15.8:3011/todos', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newTodo)
+    })
     const todos = [...this.state.todos, newTodo]
     this.setState({todos, newTodo: ''})
   }
